f7f2313e5c5d141d78a3bfe971cc6f9722e5c5a6
BruceZhang666 <yzhan475@jhu.edu>
2024-04-21 20:19:21 -0400

    segmentation inference

--- Shaobo/Finetuning DeepLabV3_small copy/src/pipeline_with_inference.py
+++ Shaobo/Finetuning DeepLabV3_small copy/src/pipeline_with_inference.py
@@ -17,6 +17,7 @@ from nearest_pixels import find_all_pairs, pairs_dict_to_list
 
 robot_color = (255, 0, 0)  # Blue color for the robot point
 human_color = (0, 255, 0)  # Green color for the human point
+deptth_threshold = 10 #threshold in cm
 
 # New code for inference and nearest pixels: 
 # Set computation device and load pre-trained model
@@ -49,9 +50,9 @@ try:
 
     # Create colorizer object
     colorizer = rs.colorizer()
-
+    count = 0
     # Streaming loop
-    for i in range(1):
+    while True:
         # Get frameset of depth
         frames = pipeline.wait_for_frames()
 
@@ -68,83 +69,91 @@ try:
         # Convert color_frame to numpy array to render image in opencv
         color_image = np.asanyarray(color_frame.get_data())
 
+        if count % 50 == 0: # infer every 50 frames
+
+            # Segmentation INference
+            # Construct the argument parser.
+
+            image = Image.fromarray(np.uint8(color_image)).convert('RGB')
 
 
+            # # Resize very large images (if width > 1024.) to avoid OOM on GPUs.
+            # if image.size[0] > 1024:
+            #     image = image.resize((800, 800))
 
+            # Do forward pass and get the output dictionary.
+            outputs = get_segment_labels(image, model, device)
+            # Get the data from the `out` key.
+            outputs = outputs['out']
+            segmented_image = draw_segmentation_map(outputs)
+            segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
 
+            # print(segmented_image.size)
+            # cv2.imshow('Segmented image', segmented_image)
+            # cv2.waitKey(5)
 
+            # Find all pairs for all images in the output directory, write to a json file
+            out_dict = find_all_pairs(segmented_image)
+            # output[masks] = out_dict
+            out_list = pairs_dict_to_list(out_dict['all_pairs'])
 
-        # New code for infernce and nearest pixels
-        # Construct the argument parser.
-
-        image = Image.fromarray(np.uint8(color_image)).convert('RGB')
-
-
-        # # Resize very large images (if width > 1024.) to avoid OOM on GPUs.
-        # if image.size[0] > 1024:
-        #     image = image.resize((800, 800))
-
-        # Do forward pass and get the output dictionary.
-        outputs = get_segment_labels(image, model, device)
-        # Get the data from the `out` key.
-        outputs = outputs['out']
-        segmented_image = draw_segmentation_map(outputs)
-        segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB)
-
-        # print(segmented_image.size)
-        # cv2.imshow('Segmented image', segmented_image)
-        # cv2.waitKey(5)
-
-        # Find all pairs for all images in the output directory, write to a json file
-        out_dict = find_all_pairs(segmented_image)
-        # output[masks] = out_dict
-        out_list = pairs_dict_to_list(out_dict['all_pairs'])
-
-
-
-
-
-
-        # pass RGB image to model
-        # points = [((100, 300), (103, 300))]
-        points = out_list
-        print(points)
-        for point in points:
-            robot_point = point[0]
-            human_point = point[1]
-            # get the depth value of the robot and human in cm
-            depth_robot = depth_frame.get_distance(robot_point[0], robot_point[1])*100
-            depth_human = depth_frame.get_distance(human_point[0], human_point[1])*100
+            # iterate to find the closest points
+            points = out_list
+            closest_pair = points[0] #closest pair of pixels
+            distance = 500
+            if points is None:
+                continue
+            print(points)
+            for point in points:
+                robot_point = point[0]
+                human_point = point[1]
+                # get the depth value of the robot and human in cm
+                depth_robot = depth_frame.get_distance(robot_point[0], robot_point[1])*100
+                depth_human = depth_frame.get_distance(human_point[0], human_point[1])*100
+                depth_diff = abs(depth_robot - depth_human)
+                if depth_diff < distance:
+                    closest_pair = point
+                    distance = depth_diff
+                if depth_diff < depth_threshold:
+                    print("too close")
+                    print("robot:", robot_point, depth_robot)
+                    print("human:", human_point, depth_human)
+                    break
+            
+            # visualize the closest pair
+            closest_robot_point, closest_human_point = closest_pair
+            closest_depth_robot = depth_frame.get_distance(closest_robot_point[0], closest_robot_point[1])*100
+            closest_depth_human = depth_frame.get_distance(closest_human_point[0], closest_human_point[1])*100
             # visualize point on the image:
-            # Visualize points on the images
-            cv2.circle(color_image, robot_point, 2, robot_color, -1)
-            cv2.circle(color_image, human_point, 2, human_color, -1)
-            cv2.circle(depth_color_image, robot_point, 2, robot_color, -1)
-            cv2.circle(depth_color_image, human_point, 2, human_color, -1)
+            cv2.circle(color_image, closest_robot_point, 2, robot_color, -1)
+            cv2.circle(color_image, closest_human_point, 2, human_color, -1)
+            cv2.circle(depth_color_image, closest_robot_point, 2, robot_color, -1)
+            cv2.circle(depth_color_image, closest_human_point, 2, human_color, -1)
 
             # Display the depth values beside the points
-            cv2.putText(color_image, f"{depth_robot:.2f}cm", (robot_point[0] + 30, robot_point[1]+30),
+            cv2.putText(color_image, f"{closest_depth_robot:.2f}cm", (closest_robot_point[0] + 30, closest_robot_point[1]+30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, robot_color, 2)
-            cv2.putText(color_image, f"{depth_human:.2f}cm", (human_point[0] - 30, human_point[1]-30),
+            cv2.putText(color_image, f"{closest_depth_human:.2f}cm", (closest_human_point[0] - 30, closest_human_point[1]-30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, human_color, 2)
-            cv2.putText(depth_color_image, f"{depth_robot:.2f}cm", (robot_point[0] + 30, robot_point[1]+30),
+            cv2.putText(depth_color_image, f"{closest_depth_robot:.2f}cm", (closest_robot_point[0] + 30, closest_robot_point[1]+30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, robot_color, 2)
-            cv2.putText(depth_color_image, f"{depth_human:.2f}cm", (human_point[0] - 30, human_point[1]-30),
+            cv2.putText(depth_color_image, f"{closest_depth_human:.2f}cm", (closest_human_point[0] - 30, closest_human_point[1]-30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, human_color, 2)
 
             # Render images in opencv windows
-            cv2.imshow("Depth Stream", depth_color_image)
-            cv2.imshow("Color Stream", color_image)
+            cv2.imshow(f"Depth Stream{count}", depth_color_image)
+            cv2.imshow(f"Color Stream{count}", color_image)
 
             # Save one frame of depth image and the corresponding RGB image
-            cv2.imwrite('saved_depth_image.png', depth_color_image)
-            cv2.imwrite('saved_color_image.png', color_image)
+            cv2.imwrite(f'saved_depth_image{count}.png', depth_color_image)
+            cv2.imwrite(f'saved_color_image{count}.png', color_image)
 
-        key = cv2.waitKey(1)
-        # if pressed escape exit program
-        if key == 27:
-            cv2.destroyAllWindows()
-            break
+            key = cv2.waitKey(1)
+            # if pressed escape exit program
+            if key == 27:
+                cv2.destroyAllWindows()
+                break
+        count += 1
 
 finally:
     pass
